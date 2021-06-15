6ac104c35fa1f50e32ec61a48abd3b9ac5200a28
João Victor de Souza Lucena <joaovictordev2003@gmail.com>
2021-06-15 14:57:07 -0300

    Usage of custom validator on coupon model is working

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -3,4 +3,5 @@ class Coupon < ApplicationRecord
   validates :discount_value, numericality: {greater_than: 0}
   enum status: {active: 1, inactive: 2}
   validates :code, uniqueness: {case_sensitive: false}
+  validates :due_date, future_date: true
 end
